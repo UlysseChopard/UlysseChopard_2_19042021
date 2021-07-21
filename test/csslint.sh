06f5d6906d435d6a133573589653b04a1abdb008
Ulysse Chopard <chopard.ulysse@gmail.com>
2021-07-21 11:56:50 +0200

    Hauteur images activites

--- test/csslint.sh
+++ test/csslint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-csslint --quiet --ignore=order-alphabetical ./css/*.css
+csslint --quiet --ignore=order-alphabetical "C:/Users/User 1/Desktop/Formation/Projet 2/css"
--- test/html-validator.sh
+++ test/html-validator.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-html-validator --file="./index.html" --verbose
+html-validator --file="C:/Users/User 1/Desktop/Formation/Projet 2/index.html" --verbose
